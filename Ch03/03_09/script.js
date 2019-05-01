7075f216e59723fbc5c74aea4b712246e664094f
Therese Karlsson <tessy_k@hotmail.com>
2019-05-01 15:20:23 +0200

    end

--- Ch03/03_09/script.js
+++ Ch03/03_09/script.js
@@ -1,74 +1,69 @@
 (function() {
-"use strict";
-
-var state = document.getElementById('s-state');
-
-document.addEventListener('DOMContentLoaded', function() {
-	document.getElementById('cart-hplus').addEventListener('submit', estimateTotal);
+	"use strict";
 	
-	var btnEstimate = document.getElementById('btn-estimate');
+	var state = document.getElementById('s-state');
 	
-	btnEstimate.disabled = true;
-	
-	state.addEventListener('change', function() {
-	
-		if (state.value === '') {
-			btnEstimate.disabled = true;
-		} else {
-			btnEstimate.disabled = false;
-		}
-	
+	document.addEventListener('DOMContentLoaded', function() {
+		document.getElementById('cart-hplus').addEventListener('submit', estimateTotal);
+		
+		var btnEstimate = document.getElementById('btn-estimate');
+		
+		btnEstimate.disabled = true;
+		
+		state.addEventListener('change', function() {
+		
+			if (state.value === '') {
+				btnEstimate.disabled = true;
+			} else {
+				btnEstimate.disabled = false;
+			}
+		
+		});
 	});
-});
-
-function estimateTotal(event) {
-	event.preventDefault();
 	
-	if (state.value === '') {
-		alert('Please choose your shipping state.');
+	function estimateTotal(event) {
+		event.preventDefault();
 		
-		state.focus();
-	}
-	
-	var itemBball = document.getElementById('txt-q-bball').value,
-		itemJersey = document.getElementById('txt-q-jersey').value,
-		itemPower = document.getElementById('txt-q-power').value,
-		shippingState = state.value,
-		shippingMethod = document.querySelector('[name=r_method]:checked').value;
+		if (state.value === '') {
+			alert('Please choose your shipping state.');
+			
+			state.focus();
+		}
 		
-	var totalQty = itemBball + itemJersey + itemPower,
-		shippingCostPer,
-		shippingCost,
-		taxFactor = 1,
-		estimate;
-		totalItemPrice =(90 * itemBball) + (25 * itemJersy) + (30 * itemPower);
-
-		if (shippingState === 'CA'){
+		var itemBball = document.getElementById('txt-q-bball').value,
+			itemJersey = document.getElementById('txt-q-jersey').value,
+			itemPower = document.getElementById('txt-q-power').value,
+			shippingState = state.value,
+			shippingMethod = document.querySelector('[name=r_method]:checked').value;
+			
+		var totalQty = itemBball + itemJersey + itemPower,
+			shippingCostPer,
+			shippingCost,
+			taxFactor = 1,
+			estimate,
+			totalItemPrice = (90 * itemBball) + (25 * itemJersey) + (30 * itemPower);
+		
+		if (shippingState === 'CA') {
 			taxFactor = 1.075;
-		} 
-
+		}
+		
 		switch(shippingMethod) {
 			case 'usps' :
 				shippingCostPer = 2;
-				break; 
+				break;
 			case 'ups' :
 				shippingCostPer = 3;
-				break; 
-			default' :
+				break;
+			default :
 				shippingCostPer = 0;
-				break; 
-
+				break;
 		}
-
+		
 		shippingCost = shippingCostPer * totalQty;
-
-		estimate = (totalItemPrice * taxFactor) +shippingCost;
-
+			
+		estimate = (totalItemPrice * taxFactor) + shippingCost;
+		
 		document.getElementById('txt-estimate').value = estimate;
 	}
 	
-
-	
-}
-
-})();
+	})();
