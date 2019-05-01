9bc2946080b46405c6407661632a87805e102b15
Therese Karlsson <tessy_k@hotmail.com>
2019-05-01 14:35:34 +0200

    end

--- Ch03/03_08/end/script.js
+++ Ch03/03_08/end/script.js
@@ -1,42 +1,42 @@
 (function() {
-"use strict";
-
-var state = document.getElementById('s-state');
-
-document.addEventListener('DOMContentLoaded', function() {
-	document.getElementById('cart-hplus').addEventListener('submit', estimateTotal);
+	"use strict";
 	
-	var btnEstimate = document.getElementById('btn-estimate');
+	var state = document.getElementById('s-state');
 	
-	btnEstimate.disabled = true;
-	
-	state.addEventListener('change', function() {
-	
-		if (state.value === '') {
-			btnEstimate.disabled = true;
-		} else {
-			btnEstimate.disabled = false;
-		}
-	
-	});
-});
-
-function estimateTotal(event) {
-	event.preventDefault();
-	
-	if (state.value === '') {
-		alert('Please choose your shipping state.');
+	document.addEventListener('DOMContentLoaded', function() {
+		document.getElementById('cart-hplus').addEventListener('submit', estimateTotal);
 		
-		state.focus();
+		var btnEstimate = document.getElementById('btn-estimate');
+		
+		btnEstimate.disabled = true;
+		
+		state.addEventListener('change', function() {
+		
+			if (state.value === '') {
+				btnEstimate.disabled = true;
+			} else {
+				btnEstimate.disabled = false;
+			}
+		
+		});
+	});
+	
+	function estimateTotal(event) {
+		event.preventDefault();
+		
+		if (state.value === '') {
+			alert('Please choose your shipping state.');
+			
+			state.focus();
+		}
+		
+		var itemBball = document.getElementById('txt-q-bball').value,
+			itemJersey = document.getElementById('txt-q-jersey').value,
+			itemPower = document.getElementById('txt-q-power').value,
+			shippingState = state.value,
+			shippingMethod = document.querySelector('[name=r_method]:checked').value;
+			
+		console.log(itemBball, itemJersey, itemPower, shippingState, shippingMethod);
 	}
 	
-	var itemBball = document.getElementById('txt-q-bball').value,
-		itemJersey = document.getElementById('txt-q-jersey').value,
-		itemPower = document.getElementById('txt-q-power').value,
-		shippingState = state.value,
-		shippingMethod = document.querySelector('[name=r_method]:checked').value;
-		
-	console.log(itemBball, itemJersey, itemPower, shippingState, shippingMethod);
-}
-
-})();
+	})();
