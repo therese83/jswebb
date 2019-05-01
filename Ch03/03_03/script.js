a58983ebd024f6a15271e9b2885b221afa6d8ce9
Therese Karlsson <tessy_k@hotmail.com>
2019-05-01 11:11:42 +0200

    End script 03_03

--- Ch03/03_03/script.js
+++ Ch03/03_03/script.js
@@ -1,6 +1,12 @@
 (function() {
-"use strict";
+    "use strict";
+    
+        var selectedIdx = document.getElementById('s-state').selectedIndex;
+        var selectedValue = document.getElementById('s-state').option[selectedIdx].value;
+    
+    })();
+
+
 
 	
 	
-})();
