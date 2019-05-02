8582b4d9f90e8402fc629fa23fe7457b24b44d65
Therese Karlsson <tessy_k@hotmail.com>
2019-05-02 09:55:50 +0200

    vardate

--- Ch04/04_01/script.js
+++ Ch04/04_01/script.js
@@ -1,6 +1,17 @@
 (function(){
 'use strict';
 
+document.addEventListener('DOMContentLoaded', function(){
+    var c = document.getElementById('current-time');
+
+    var d = new Date();
+    
+    c.innerHTML = d.toTimeString();
+
+    c.innerHTML = '1:00 PM';
+
+    });
+
 
 
 })();
